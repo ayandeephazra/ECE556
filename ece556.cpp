513784c279af596c11f9d5b6ee1e16ad158e14fe
Ayan Deep Hazra <ahazra2@wisc.edu>
2023-03-26 22:34:35 -0500

    savestate

--- ece556.cpp
+++ ece556.cpp
@@ -11,6 +11,7 @@ int readBenchmark(const char *fileName, routingInst *rst)
 
   int num_blockages;
   char tokena[100], tokenb[100], tokenc[100], tokend[100], tokene[100];
+  char token1[100], token2[100], token3[100], token4[100], token5[100], token6[100], token7[100];
   int one, two, three, four, five;
 
   FILE *file;
@@ -21,145 +22,147 @@ int readBenchmark(const char *fileName, routingInst *rst)
   }
 
   char *buffer = (char *)malloc(200 * sizeof(char *)); // buffer can store 200 characters
-  rst = (routingInst *)malloc(sizeof(routingInst));
+  // rst = (routingInst *)malloc(sizeof(routingInst));
 
   // while loop to start parsing file
   // assuming a line is no longer than 200 chars
+  int t = 0;
+  int phase = 1;
+
   while (fgets(buffer, 200 * sizeof(char), file))
   {
     // keyword, 1st number, 2nd number
-    char token1[100], token2[100], token3[100], token4[100], token5[100], token6[100], token7[100];
-    // data variables
-    int phase = 1;
 
-    if ((sscanf(buffer, "%s", token1) > 0) & (phase == 1))
+    // data variables
+
+    if ((sscanf(buffer, "%s", token1) > 0))
     {
+
+      if (t == 0 | t == 1)
+      {
+        // printf("%s", token1);
+        // t++;
+      }
 
       ///////////////////////////////////////////////////
       //   grid parsing                               //
       /////////////////////////////////////////////////
-      if (strcmp(token1, "grid") == 0)
+      if ((phase == 1) & strcmp(token1, "grid") == 0)
       {
-        char *x = (char *)malloc(sizeof(char));
-        if (sscanf(buffer, "%s", x) > 0)
+        // printf("%s", token1);
+        char x_[100], y_[100];
+        if (sscanf(buffer, "%s %s %s\n", token1, x_, y_) > 0)
         {
-          rst->gx = atoi(x);
+          rst->gx = atoi(x_);
+          rst->gy = atoi(y_);
         }
-        free(x);
-        char *y = (char *)malloc(sizeof(char));
-        if (sscanf(buffer, "%s", y) > 0)
-        {
-          rst->gy = atoi(y);
-        }
-        free(y);
-
         phase = 2;
+        continue;
       }
-
       ///////////////////////////////////////////////////
       //   capacity parsing                           //
       /////////////////////////////////////////////////
-      else if ((strcmp(token1, "capacity") == 0) & (phase == 2))
+      else if ((phase == 2) & (strcmp(token1, "capacity") == 0))
       {
-        char *capacity = (char *)malloc(sizeof(char));
-        if ((sscanf(buffer, "%s", capacity)) > 0)
+        char capacity[100];
+        if ((sscanf(buffer, "%s %s\n", token1, capacity)) > 0)
         {
           rst->cap = atoi(capacity);
         }
-        free(capacity);
         phase = 3;
+        continue;
       }
 
       ///////////////////////////////////////////////////
       //   nets parsing                               //
       /////////////////////////////////////////////////
-      else if ((strcmp(token1, "num") == 0) & (phase == 3))
+      else if ((phase == 3) & (strcmp(token1, "num") == 0))
       {
-        if ((sscanf(buffer, "%s", token2) > 0) & (strcmp(token2, "nets") == 0))
+        char nnets[100];
+        if ((sscanf(buffer, "%s %s %s\n", token1, token2, nnets) > 0) & (strcmp(token2, "net") == 0))
         {
-          char *numnets = (char *)malloc(sizeof(char));
-          if (sscanf(buffer, "%s", numnets) > 0)
-          {
-            rst->numNets = atoi(numnets);
-          }
-          free(numnets);
+          rst->numNets = atoi(nnets);
         }
 
         // instantiate nets filed in rst
         rst->nets = (net *)malloc(rst->numNets * sizeof(net));
         phase = 4;
+        continue;
       }
+    }
+  }
 
-      ///////////////////////////////////////////////////
-      //   initialization of nets struct within rst   //
-      /////////////////////////////////////////////////
-      else if (phase == 4)
+  ///////////////////////////////////////////////////
+  //   initialization of nets struct within rst   //
+  /////////////////////////////////////////////////
+  if (phase == 4)
+  {
+    for (int i = 0; i < rst->numNets; i++)
+    {
+      rst->nets[i].id = i;
+      if ((sscanf(buffer, "%s %s\n", token3, token4) > 0))
       {
-        for (int i = 0; i < rst->numNets; i++)
+        // printf("%s", token4);
+        rst->nets[i].numPins = atoi(token4);
+      }
+      // declaring the points/pins in each net
+      rst->nets[i].pins = (point *)malloc(rst->nets[i].numPins * sizeof(point));
+
+      for (int j = 0; j < rst->nets[i].numPins; j++)
+      {
+        // printf("this%d", j);
+        fgets(buffer, 200 * sizeof(char), file);
+        printf(buffer);
+
+        if (sscanf(buffer, "%s %s\n", token5, token6) > 0)
         {
-          rst->nets[i].id = i;
-          if ((sscanf(buffer, "%s", token3) > 0) & (sscanf(buffer, "%s", token4) > 0))
-          {
-            rst->nets[i].numPins = atoi(token4);
-          }
-          // declaring the points/pins in each net
-          rst->nets[i].pins = (point *)malloc(rst->nets[i].numPins * sizeof(point));
-
-          for (int j = 0; j < rst->nets[i].numPins; j++)
-          {
-            fgets(buffer, 200 * sizeof(char), file);
-
-            if ((sscanf(buffer, "%s", token5) > 0) & (sscanf(buffer, "%s", token6) > 0))
-            {
-              rst->nets[i].pins[j].x = atoi(token5);
-              rst->nets[i].pins[j].y = atoi(token6);
-            }
-          }
-          // route malloc
-          // will be assigned in solveRouting
-
-          ////////////////////////////////////////////////////////////////
-          //   Performs blockage parsing. Assuming that bad output      //
-          //   is not a real scenario and so in that case, blockage     //
-          //   statements parse the single integer in the following     //
-          //   line after the last nets' pins. Uses that in a for loop  //
-          //   to cycle through the five integer line input of the      //
-          //   blockages and add to rst instance aptly.                 //
-          ////////////////////////////////////////////////////////////////
+          rst->nets[i].pins[j].x = atoi(token5);
+          rst->nets[i].pins[j].y = atoi(token6);
         }
-        phase = 5;
       }
-      else
+      // route malloc
+      // will be assigned in solveRouting
+
+      ////////////////////////////////////////////////////////////////
+      //   Performs blockage parsing. Assuming that bad output      //
+      //   is not a real scenario and so in that case, blockage     //
+      //   statements parse the single integer in the following     //
+      //   line after the last nets' pins. Uses that in a for loop  //
+      //   to cycle through the five integer line input of the      //
+      //   blockages and add to rst instance aptly.                 //
+      ////////////////////////////////////////////////////////////////
+    }
+    phase = 5;
+  }
+  else
+  {
+    if (sscanf(buffer, "%s", token7) > 0)
+    {
+      num_blockages = atoi(token7);
+
+      for (int k = 0; k < num_blockages; k++)
       {
-        if (sscanf(buffer, "%s", token7) > 0)
+        fgets(buffer, 200 * sizeof(char), file);
+
+        if (sscanf(buffer, "%s", tokena) > 0)
         {
-          num_blockages = atoi(token7);
-
-          for (int k = 0; k < num_blockages; k++)
-          {
-            fgets(buffer, 200 * sizeof(char), file);
-
-            if (sscanf(buffer, "%s", tokena) > 0)
-            {
-              one = atoi(tokena);
-            }
-            if (sscanf(buffer, "%s", tokenb) > 0)
-            {
-              two = atoi(tokenb);
-            }
-            if (sscanf(buffer, "%s", tokenc) > 0)
-            {
-              three = atoi(tokenc);
-            }
-            if (sscanf(buffer, "%s", tokend) > 0)
-            {
-              four = atoi(tokend);
-            }
-            if (sscanf(buffer, "%s", tokene) > 0)
-            {
-              five = atoi(tokene);
-            }
-          }
+          one = atoi(tokena);
+        }
+        if (sscanf(buffer, "%s", tokenb) > 0)
+        {
+          two = atoi(tokenb);
+        }
+        if (sscanf(buffer, "%s", tokenc) > 0)
+        {
+          three = atoi(tokenc);
+        }
+        if (sscanf(buffer, "%s", tokend) > 0)
+        {
+          four = atoi(tokend);
+        }
+        if (sscanf(buffer, "%s", tokene) > 0)
+        {
+          five = atoi(tokene);
         }
       }
     }
@@ -283,6 +286,7 @@ int writeOutput(const char *outRouteFile, routingInst *rst)
     perror("Error opening file");
     return 0;
   }
+  fprintf(file, "%d", rst->numNets);
   for (int i = 0; i < rst->numNets; i++)
   {
     fprintf(file, "%s%d\n", "n", i);
